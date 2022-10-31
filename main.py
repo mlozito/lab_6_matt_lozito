09b71599d21831bdd796a33fd06fbc6e3ca5eb89
Matthew Lozito <matt.lozito@gmail.com>
2022-10-31 17:29:00 -0400

    added subtraction and menu

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-user_input_1 = int(input("Please select your first integer.", ))
-user_input_2 = int(input("Please select your second integer.", ))
-sum = user_input_1 + user_input_2
-print(f"The sum of your two integers is {sum}.")
+print("Hello!\n"
+      "------")
+while True:
+    print("0. Quit\n"
+          "1. Add\n"
+          "2. Subtract\n")
+    user_operation = int(input("What would you like to do?", ))
+    if user_operation == 0:
+        quit()
+    if user_operation == 1:
+        user_input_1 = int(input("Please select your first integer.", ))
+        user_input_2 = int(input("Please select your second integer.", ))
+        sum = user_input_1 + user_input_2
+        print(f"The sum of your two integers is {sum}.\n")
+    if user_operation == 2:
+        user_input_1 = int(input("Please select your first integer.", ))
+        user_input_2 = int(input("Please select your second integer.", ))
+        difference = user_input_1 - user_input_2
+        print(f"The difference of your two integers is {difference}.\n")

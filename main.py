b6ded66da015dc80ab50b03294ae7f43f031e94a
Matthew Lozito <matt.lozito@gmail.com>
2022-10-31 17:19:06 -0400

    added code

---
+++ main.py
@@ -0,0 +1,4 @@
+user_input_1 = int(input("Please select your first integer.", ))
+user_input_2 = int(input("Please select your second integer.", ))
+sum = user_input_1 + user_input_2
+print(f"The sum of your two integers is {sum}.")
